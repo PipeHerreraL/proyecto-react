ac91698b52c68147a3f280dc94177d6c0ed64312
Juan Felipe López <99980782+PipeHerreraL@users.noreply.github.com>
2025-04-29 22:56:09 -0500

    Update UserForm.jsx

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -7,32 +7,32 @@ const UserForm = ({ handleChange }) => {
     return (
         <form className='px-8 py-4 border rounded-md'>
                     
-                    <FormInput
-                    label="Name"
-                    type="text"
-                    name="name"
-                    onChange={handleChange}
-                    />
+            <FormInput
+            label="Name"
+            type="text"
+            name="name"
+            onChange={handleChange}
+            />
 
-                    <FormInput
-                    label="Description"
-                    type="text"
-                    name="description"
-                    onChange={handleChange}
-                    />
+            <FormInput
+            label="Description"
+            type="text"
+            name="description"
+            onChange={handleChange}
+            />
 
-                    <FormInput
-                    label="Age"
-                    type="range"
-                    name="age"
-                    />
+            <FormInput
+            label="Age"
+            type="range"
+            name="age"
+            />
 
-                    <FormInput
-                    label="Profile picture"
-                    type="file"
-                    name="profilePicture"
-                    onChange={handleChange}
-                    />
+            <FormInput
+            label="Profile picture"
+            type="file"
+            name="profilePicture"
+            onChange={handleChange}
+            />
 
             <div className='mt-8'>
                 <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Save</button>
