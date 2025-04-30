d0f5849ae54333a02c62dcd3a4d4f62abdbbc1e8
Juan Felipe López <99980782+PipeHerreraL@users.noreply.github.com>
2025-04-29 20:35:42 -0500

    add form in welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,12 +3,36 @@ import Profile from "../components/Profile";
 
 const Welcome = () => {
     return (
-        <div className='flex gap-4 justify-center mt-6'>
-            <Profile image="https://cdn.pixabay.com/photo/2023/01/19/10/24/castle-7728772_1280.jpg" />
-            <Profile image="https://cdn.pixabay.com/photo/2023/01/22/15/16/ocean-7736669_1280.jpg" />
-            <Profile image="https://cdn.pixabay.com/photo/2023/01/25/18/14/landscape-7744216_1280.jpg" />
+        <div className="flex justify-center mt-6">
+            {/* Tarjeta */}
+            <div className="w-1/3">
+                <Profile image="https://cdn.pixabay.com/photo/2023/01/19/10/24/castle-7728772_1280.jpg" />
+            </div>
+
+            {/* Formulario */}
+            <div className="w-1/3">
+                <form className='px-8 py-4 border rounded-md'>
+                    
+                    <span className="block text-sm font-medium text-slate-700">Name</span>
+                    <input type="text" className="px-4 py-2 border rounded-md w-full" placeholder='Name'/>
+
+                    <span className="block text-sm font-medium text-slate-700 mt-6">Last name</span>
+                    <input type="text" className="px-4 py-2 border rounded-md w-full" placeholder='Last Name'/>
+
+                    <span className="block text-sm font-medium text-slate-700 mt-6">Age</span>
+                    <input type="range" className="px-4 py-2 border rounded-md w-full" placeholder='Age'/>
+
+                    <span className="block text-sm font-medium text-slate-700 mt-6">Profile picture</span>
+                    <input type="file" className="px-4 py-2 border rounded-md w-full" placeholder='Profile picture' />
+
+                    <div className='mt-8'>
+                        <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Save</button>
+                    </div>
+                    
+                </form>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Welcome;
