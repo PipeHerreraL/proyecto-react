141fe961076eb4c2ef095e52d3be2529aca1025c
Juan Felipe López <99980782+PipeHerreraL@users.noreply.github.com>
2025-04-29 21:19:17 -0500

    refactor show/hide button

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -39,35 +39,21 @@ const Welcome = () => {
 
                 </form>
 
-                <div className='m-10'>
-                    {showButton && (
-                        <button
-                            type="button"
-                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                            onClick={() => {
-                                    setIsStudent(true);
-                                    setShowButton(false);
-                                }}
-                        >
-                        Show text
-                        </button>
-                    )}
-                    {!showButton && (
-                        <button
-                            type="button"
-                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                            onClick={() => {
-                                    setIsStudent(false);
-                                    setShowButton(true);
-                                }}
-                        >
-                        Hide text
-                        </button>
-                    )}
-                        {
-                            isStudent && <p>Welcome 😃</p>
-                        }
+                <div className="m-10">
+                    <button
+                        type="button"
+                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                        onClick={() => {
+                        setIsStudent(!isStudent);
+                        setShowButton(!showButton);
+                        }}
+                    >
+                        {showButton ? 'Show text' : 'Hide text'}
+                    </button>
+
+                    {isStudent && <p>Welcome 😃</p>}
                 </div>
+
             </div>
         </div>
     );
