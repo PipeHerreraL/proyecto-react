450337a04c376c034d5e2d5ebb5fc256f7e0a458
Juan Felipe López <99980782+PipeHerreraL@users.noreply.github.com>
2025-04-29 21:18:21 -0500

    RETO 3: create 'hide text' button

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import Profile from "../components/Profile";
 
 const Welcome = () => {
+
+    const [isStudent, setIsStudent] = useState(false);
+    const [showButton, setShowButton] = useState(true);
+
     return (
         <div className="flex justify-center mt-6">
             {/* Tarjeta */}
@@ -34,6 +38,36 @@ const Welcome = () => {
                     </div>
 
                 </form>
+
+                <div className='m-10'>
+                    {showButton && (
+                        <button
+                            type="button"
+                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                            onClick={() => {
+                                    setIsStudent(true);
+                                    setShowButton(false);
+                                }}
+                        >
+                        Show text
+                        </button>
+                    )}
+                    {!showButton && (
+                        <button
+                            type="button"
+                            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                            onClick={() => {
+                                    setIsStudent(false);
+                                    setShowButton(true);
+                                }}
+                        >
+                        Hide text
+                        </button>
+                    )}
+                        {
+                            isStudent && <p>Welcome 😃</p>
+                        }
+                </div>
             </div>
         </div>
     );
